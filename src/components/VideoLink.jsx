f84b7d3360ec2a703802734e87c534665107ec80
theo <thjmaurino@gmail.com>
2025-04-25 16:52:15 -0500

    bigger thumbnails, correct terrarium link

--- src/components/VideoLink.jsx
+++ src/components/VideoLink.jsx
@@ -6,7 +6,7 @@ const VideoLink = ({url, image_path, title}) => {
 
   return (
     <Link href={url} target="_blank">
-        <div className='video-link w-[400px] h-[300px] flex flex-col gap-5'>
+        <div className='video-link w-[450px] h-[350px] flex flex-col gap-5'>
             <p className='text-'>{title}</p>
             <img src={image_path} className="object-cover max-w-full max-h-full  ring ring-2 ring-gray-200 ring-offset-4 rounded-sm" />
         </div>
